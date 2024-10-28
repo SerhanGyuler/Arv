7157e71d0a99ec6345e889a28d9ac7529a61351f
Serhan Gyuler <serhan.gyuler@hotmail.com>
2024-10-28 19:16:52 +0100

    Create BaseClass

---
+++ Arv/Animal.cs
@@ -0,0 +1,28 @@
+﻿namespace Arv
+{
+    internal class Animal
+    {
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public string Habitat { get; set; }
+        public string Diet { get; set; }
+
+
+        public void Eat()
+        {
+
+        }
+
+        public void Sleep()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+
+    }
+}
